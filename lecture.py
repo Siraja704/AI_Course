a4dcc77a38f17df49fe759cbe0d7349ca07f71c9
Siraja704 <sirajahmedmemon.bscsf22@iba-suk.edu.pk>
2025-04-21 12:03:38 +0500

    Problems added

---
+++ lecture.py
@@ -0,0 +1,18 @@
+
+# name = input( " Enter you name : " )
+
+age = input("I want you to add your age ")
+
+
+
+print(type(age))
+
+# print("your name is :" , name)
+
+# print("your age is " )
+
+# casting
+# str to int 
+# float to int
+#int to str
+# int to float 
